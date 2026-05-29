20fab5e2a9279d7da46305a5c34ba3a720cf2d21
VenuraDenethpriya <venurameedum@gmail.com>
2026-05-30 01:03:37 +0530

    Fix import statements to include file extensions for consistency

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,14 +1,14 @@
 import express from 'express';
 import 'dotenv/config';
-import { productRouter } from './api/product';
+import { productRouter } from './api/product.js';
 import globalErrorHanlingMiddleware from './api/middleware/global-error-handling-middleware';
-import { categoryRouter } from './api/category';
+import { categoryRouter } from './api/category.js';
 import { connectDB } from './infrastructure/db';
 import cors from 'cors';
-import { orderRouter } from './api/order';
+import { orderRouter } from './api/order.js';
 import { clerkMiddleware } from '@clerk/express';
-import { paymentRouter } from './api/payment';
-import { userRouter } from './api/user';
+import { paymentRouter } from './api/payment.js';
+import { userRouter } from './api/user.js';
 import { handleWebhook } from './application/payment';
 import bodyParser from 'body-parser';
 
